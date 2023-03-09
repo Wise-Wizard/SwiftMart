ecfbd2325085aa7798152a515e76f2a5c48c9f1e
Wise-Wizard <saransh.shankar@gmail.com>
2023-03-09 23:18:14 +0530

    Refactored Code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,23 +11,27 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Node</h1>");
+  // Changed from 'router' to 'app' as it is an Express route handler and not a Router route handler.
+  res.send("<h1>Welcome to Node</h1>"); // Added a closing tag for h1 element as this is invalid HTML syntax without it.
 });
+
 app.get("/products", (req, res) => {
   res.json(products);
 });
+
 app.get("/products/:id", (req, res) => {
   const product = products.find((p) => {
     return p._id === req.params.id;
   });
+
   res.json(product);
 });
+
 app.listen(process.env.PORT, () => {
   console.log("Server is Up and Running.");
 });
