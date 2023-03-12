a84fb96c5235fdf5d4bb392930576a5c2e49a001
Wise-Wizard <saransh.shankar@gmail.com>
2023-03-12 16:07:45 +0530

    Added Cart Redux feature

--- FrontEnd/react-app/src/store.js
+++ FrontEnd/react-app/src/store.js
@@ -3,12 +3,22 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import productListReducer from "./Reducers/productsReducer";
 import productDetailsReducer from "./Reducers/productDetailsReducer";
+import { cartReducer } from "./Reducers/cartReducer";
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  },
+};
 const middleWare = [thunk];
 const store = createStore(
   reducer,
